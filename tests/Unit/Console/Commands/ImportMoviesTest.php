d10f388a0e05b7ee83b6a8979db3bf1e439bd23b
umair68 <umair68@mail.com>
2024-08-11 01:12:43 +0200

    update: ImportMoviesTest to include genres

--- tests/Unit/Console/Commands/ImportMoviesTest.php
+++ tests/Unit/Console/Commands/ImportMoviesTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\Unit\Console\Commands;
 
+use App\Models\Genre;
 use App\Models\Movie;
 
 final class ImportMoviesTest extends \Tests\TestCase
@@ -12,10 +13,12 @@ final class ImportMoviesTest extends \Tests\TestCase
     {
 
         $this->assertEquals(0, Movie::count());
+        $this->assertEquals(0, Genre::count());
 
         $this->artisan('import:movies --limit=10')
             ->assertExitCode(0);
 
         $this->assertEquals(10, Movie::count());
+        $this->assertGreaterThan(0, Genre::count());
     }
 }
