17602d5f958205f19a82095a2c2621473e5ea4fb
umair68 <umair68@mail.com>
2024-08-15 00:41:35 +0200

    add: HomeController wip

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,11 @@
 
 declare(strict_types=1);
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn () => view('welcome'));
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
 
 Route::get('/dashboard', fn () => view('dashboard'))->middleware(['auth', 'verified'])->name('dashboard');
 
